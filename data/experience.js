ef04a10e24c7adacf23078c70efa32a12be7f46e
Phillip Eade <phillipeade@Phillips-MacBook-Air.local>
2022-05-22 17:00:38 +1000

    Fixed data

---
+++ data/experience.js
@@ -0,0 +1,41 @@
+const experience = [
+    {
+        title: "Software Developer",
+        company: "Mudbath Digital",
+        year: "2020",
+        companyLink: "https://www.mudbath.com.au/",
+        desc: "Working across many different tech stacks, I also consulted inside our clients teams, which included The Greater Bank and NIB.",
+      },
+      {
+        title: "Junior Web and Integration Developer",
+        company: "The University of Newcastle",
+        year: "2018",
+        companyLink: "https://www.newcastle.edu.au/m",
+        desc: "Primarily worked inside the myUON app working with ReactJS and .Net. I also tech lead and mentored grad developers.",
+      },        {
+        title: "Grad Junior Web and Integration Developer",
+        company: "The University of Newcastle",
+        year: "2016",
+        companyLink: "https://www.newcastle.edu.au/",
+        desc: "Worked on many .Net and ReactJS projects",
+      },      
+      {
+        title: "Freelance Web Developer",
+        company: "Self",
+        year: "2016",
+        companyLink: "www.PhillipEade.com",
+        desc: "Worked on a few freelance projects for people and startups, I had to both manage the projects and develop the architecture from scratch.",
+      },   
+      {
+        title: "Student",
+        company: "Master of IT - Univeristy of Newcastle",
+        year: "2016",
+      },        {
+        title: "Student",
+        company: "Bachelor of Computer Science - University of Newcastle",
+        year: "2007",
+      },
+];
+
+
+export default experience;
